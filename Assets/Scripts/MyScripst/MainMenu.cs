ce18ca581c0fccbb275253105e9f4ba76032b2fc
Cyppz <michel.olimart@outlook.de>
2026-07-02 14:24:35 +0200

    new scripts not complete yet
    scripts and basic architecture  stands, not quite complete.

--- Assets/Scripts/MyScripst/MainMenu.cs
+++ Assets/Scripts/MyScripst/MainMenu.cs
@@ -4,16 +4,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] SaveManager saveManager;
     [SerializeField] TextMeshProUGUI playerName;
     string _playerName;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
@@ -21,6 +17,11 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
         _playerName = playerName.text;
+
+        if (_playerName == null || _playerName == string.Empty || _playerName == " ")
+            return;
+        
+        saveManager.SetCurrentPlayer(_playerName, 0); //savemanager handles checking for existing
         SceneManager.LoadScene(1);
         Debug.Log(_playerName);
     }
--- Assets/Scripts/MyScripst/SaveManager.cs
+++ Assets/Scripts/MyScripst/SaveManager.cs
@@ -1,16 +1,123 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Playables;
+using System.IO;
+using System.Linq;
 
 public class SaveManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    SaveData saveData; //should be created on first time saving, then always laoded on start.
+
+    PlayerData currentPlayer;
+    PlayerData currentPlayerInData; //set this to the corresponding entry if player has played before
+
+    PlayerData highScorePlayer = new PlayerData("None", 0);
+
+    private void Awake()
     {
-        
+        DontDestroyOnLoad(gameObject);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Load();
+        if (saveData == null) //if no data is found, create data and fill with dummy player called NONE
+        {
+            saveData = new SaveData();
+            saveData.dataList = new List<PlayerData>();
+            saveData.dataList.Add(highScorePlayer);
+        }
+        GetHighscore();
+    }
+
+    void UpdateHighScorePlayer(PlayerData p)
+    {
+        highScorePlayer = p;
+    }
+
+    void GetHighscore()
+    {
+        //after loading the savedata, sor by scrore and get the last item from the list
+        //this has to be the highest score.
+        var sorted = from player in saveData.dataList
+                     orderby player.highscore descending
+                     select player;
+
+        highScorePlayer = sorted.First();
+    }
+
+    public void SetCurrentPlayer(string name, int score)
+    {
+        name = name.ToLower();
+        string pName = char.ToUpper(name[0]) + name.Substring(1);
+        if (pName.Length > 10)
+        {
+            pName = pName.Substring(0, 10);
+        }
+
+        PlayerData p = new PlayerData(pName, score);
+        currentPlayer = p;
+
+        if (CheckExisting(pName, out PlayerData existing))
+        {
+            currentPlayerInData = existing;
+        }
+    }
+
+    bool CheckExisting(string name, out PlayerData existing)
+    {
+        foreach (PlayerData p in saveData.dataList)
+        {
+            if (p.playerName == name)
+            {
+                existing = p;
+                return true;
+            }
+        }
+        existing= null;
+        return false;
+    }
+
+    public void UpdateCurrentScore(int score)
+    {
+        currentPlayer.highscore = score;
+    }
+    
+    void Save()
+    {
+        string dataJson = JsonUtility.ToJson(saveData);
+        File.WriteAllText(Application.persistentDataPath + "/SaveData.json", dataJson);
+    }
+
+    void Load()
+    {
+        string path = Application.persistentDataPath + "/SaveData.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            saveData = JsonUtility.FromJson<SaveData>(json);
+        }
+    }
+
+
+    [System.Serializable] 
+    public class SaveData
+    {
+        public List<PlayerData> dataList = new List<PlayerData>();
+    }
+
+    [System.Serializable]
+    public class PlayerData
+    {
+        public string playerName;
+        public int highscore;
+
+        public PlayerData(string name, int score)
+        {
+            playerName = name;
+            highscore = score;
+        }
     }
 }
